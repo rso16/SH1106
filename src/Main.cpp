99585faef89a93360cce38a90b27cdbef23d7e74
rso16 <rick.overhorst@gmail.com>
2017-07-01 13:14:44 +0200

    added settings, it works now.

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,25 +13,43 @@ int main() {
   SH1106 s;
   s.sendStart();
   s.sendAddr(SH1106_addr);
+  s.sendCommand(0x0ae);
+  s.sendCommand(0x0d5);
+  s.sendCommand(0x80);
+  s.sendCommand(0x08d);
+  s.sendCommand(0x14);
+  s.sendCommand(0x20);
+  s.sendCommand(0x00);
+  s.sendCommand(0xA1);
+  s.sendCommand(0xC8);
+  // s.sendCommand();
+  // s.sendCommand();
+  // s.sendCommand();
+  // s.sendCommand();
+  // s.sendCommand();
   s.sendCommand(0xDA);
-  s.sendCommand(0x02);
+  s.sendCommand(0x12);
   s.sendCommand(0xA8); // set multiplex
-  s.sendCommand(0x00); // to 0
+  s.sendCommand(0x3f); // to 0
   s.sendCommand(0xD3); // set offset
-  s.sendCommand(0x01); // to the top of the screen
+  s.sendCommand(0x00); // to the top of the screen
   s.sendCommand(0x40); // set line to 0
   s.sendCommand(0xB0); // set page to 0
   s.sendCommand(0x02); // set collom lower to 2
-  s.sendCommand(0x10); // set collom lower to 0
+  s.sendCommand(0x10); // set collom upper to 0
   s.sendCommand(0xAF); // turn screen on
 
 // should draw 5 lines but only the first is drawn.
-    for (size_t j = 0; j <5; j++)
+    for (size_t j = 0; j <=7; j++)
     {
-      for (size_t i = 0; i < 132; i++)
+      s.sendCommand(0xB0+j);
+      s.sendCommand(0x02); // set collom lower to 2
+      s.sendCommand(0x10); // set collom upper to 0
+
+      for (size_t i = 0; i < 128; i++)
       {
-        s.sendRAM(0xFF);
-        _delay_ms(10);
+        s.sendRAM(0xAA);
+        _delay_ms(50);
       }
       _delay_ms(500);
     }
