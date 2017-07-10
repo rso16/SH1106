d05450beae4087e05a7bbd09c58fb803daeaa4ab
rso16 <rick.overhorst@gmail.com>
2017-07-10 15:23:01 +0200

    stuff

--- lib/SH1106/SH1106.cpp
+++ lib/SH1106/SH1106.cpp
@@ -1,32 +1,32 @@
 #include "SH1106.h"
 //made by Rick Overhorst
 //varius handy dandy fuction for the atmel atmega 328 chip/Arduino
-    void SH1106::init()
-    {
-      sendStart();
-      sendAddr(SH1106_ADDR);
-      sendCommand(0x0ae);
-      sendCommand(0x0d5);
-      sendCommand(0x80);
-      sendCommand(0x08d);
-      sendCommand(0x14);
-      sendCommand(0x20);
-      sendCommand(0x00);
-      sendCommand(0xA1);
-      sendCommand(0xC8);
-      sendCommand(0xDA);
-      sendCommand(0x12);
-      sendCommand(0xA8); // set multiplex
-      sendCommand(0x3f); // to 0
-      sendCommand(0xD3); // set offset
-      sendCommand(0x00); // to the top of the screen
-      sendCommand(0x40); // set line to 0
-      sendCommand(0xB0); // set page to 0
-      sendCommand(0x02); // set collom lower to 2
-      sendCommand(0x10); // set collom upper to 0
-      sendCommand(0xAF); // turn screen on
-      sendStop();
-    }
+void SH1106::init()
+{
+  sendStart();
+  sendAddr(SH1106_ADDR);
+  sendCommand(0x0ae);
+  sendCommand(0x0d5);
+  sendCommand(0x80);
+  sendCommand(0x08d);
+  sendCommand(0x14);
+  sendCommand(0x20);
+  sendCommand(0x00);
+  sendCommand(0xA1);
+  sendCommand(0xC8);
+  sendCommand(0xDA);
+  sendCommand(0x12);
+  sendCommand(0xA8); // set multiplex
+  sendCommand(0x3f); // to 0
+  sendCommand(0xD3); // set offset
+  sendCommand(0x00); // to the top of the screen
+  sendCommand(0x40); // set line to 0
+  sendCommand(0xB0); // set page to 0
+  sendCommand(0x02); // set collom lower to 2
+  sendCommand(0x10); // set collom upper to 0
+  sendCommand(0xAF); // turn screen on
+  sendStop();
+}
 
     void SH1106::sendStart()
     {
@@ -110,7 +110,6 @@
       for (size_t i = 0; i < oledHight * oledWidth; i++) {
         Serial.println(buffer[i]);
       }
-
     }
     uint8_t SH1106::getBit(uint8_t data, uint8_t index)
     {
