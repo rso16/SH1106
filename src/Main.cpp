6b9f4a038e6ab9ed51c8b535adf7d4fbd1514e90
KevinJan18 <kevin@vjkd.nl>
2017-07-10 21:00:11 +0200

    Possible Fix

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,36 +10,6 @@
 uint8_t buffer[oledHight * oledWidth];
 //#define RTC_ADDRESS 0x68
 int main() {
-<<<<<<< HEAD
-  init();
-  Serial.begin(9600);
-  SH1106 s;
-  s.init();
-//   s.sendStart();
-//   s.sendAddr(SH1106_ADDR);
-// // should draw 5 lines but only the first is drawn.
-//     for (size_t j = 0; j <=0; j++)
-//     {
-//       s.sendCommand(0xB0+j);
-//       s.sendCommand(0x12); // set collom lower to 2
-//       s.sendCommand(0x10); // set collom upper to 0
-//
-//       for (size_t i = 0; i < 128; i++)
-//       {
-//         s.sendRAM(0x00);
-//         //_delay_ms(100);
-//       }
-//       // _delay_ms(00);
-//     }
-//     //_delay_ms(1000);
-//     s.sendStop();
-  while(1){
-    s.fillBuffer(0x00, buffer);
-    s.sendStart();
-    s.DrawBuffer(buffer);
-    s.sendStop();
-  }
-=======
 init();
 Serial.begin(9600);
 // Serial.println("blyat");
@@ -56,5 +26,4 @@ Serial.println("blyat");
 //s.sendStop();
 _delay_ms(1000);
   return 0;
->>>>>>> d05450beae4087e05a7bbd09c58fb803daeaa4ab
 }
