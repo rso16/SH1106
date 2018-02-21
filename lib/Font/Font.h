cc26b1b2ac398b541ffbfcd83ca3c7865f2531da
rso16 <rick.overhorst@gmail.com>
2018-02-21 23:08:49 +0100

    font constructor error

--- lib/Font/Font.h
+++ lib/Font/Font.h
@@ -5,15 +5,18 @@
 #include <stdint.h>
 #include <string.h>
 #include "Character.h"
+
+#define maxChars 50
 class Font
 {
   private:
-    Character font[50];
-    int fontCounter;
+    Character font[maxChars];
+    int fontCounter = 0;
   public:
     Font();
     void addChar(Character c);
     Character getChar(char c);
+    int findFontIndex(char c);
     void printChars();
 };
 
